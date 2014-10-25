5826b297fc3067934e0788b9a25e8f664e4cf3ce
phy-x <bc.zhu@qq.com>
2014-10-26 00:30:50 +0800

    add

--- files/js/calendar.js
+++ files/js/calendar.js
@@ -21,7 +21,7 @@ document.write(dayName[t.getDay()]+'<br /><span class="day">'+t.getDate()+"</spa
 else{
 dayName=new Array("星期日","星期一","星期二","星期三","星期四","星期五","星期六");
 monthName=new Array("一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月");
-yearName=new Array("零","一","二","三","四","五","六","七","八","九");
+yearName=new Array("〇","一","二","三","四","五","六","七","八","九");
 var n=t.getFullYear().toString();
 var r=yearName[n.charAt(0)]+yearName[n.charAt(1)]+yearName[n.charAt(2)]+yearName[n.charAt(3)]+"年";
 document.write('<div><div style="padding-bottom: 5px">'+dayName[t.getDay()]+'</div><span class="day">'+t.getDate()+"</span><br />"+r+monthName[t.getMonth()]+'</div><span class="daynumber"> page '+'<span style="color:red">'+daynum+"</span> of "+daynumt+"<br /> chapter "+t.getFullYear()+"<br /></span>")}}
